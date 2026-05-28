ef7df60226b05728030f2dfc45cecf0ada9f4c3e
Ayush Patel <ayushpatel5779@gmail.com>
2026-05-28 10:52:15 +0530

    fix email port to 587

--- portfolio-backend/utils/sendEmail.js
+++ portfolio-backend/utils/sendEmail.js
@@ -1,7 +1,10 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // false for port 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
